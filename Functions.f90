9e9b693e9d3444ce3c9a4f3a53397e4533472a8f
Murilodsv <murilodsv@usp.br>
2018-02-02 12:12:42 -0200

    Code Implemented

--- Functions.f90
+++ Functions.f90
@@ -1,1 +1,30 @@
-
+!----------------------------------------------------------------------------------------
+      real function AScurv(df,x,min,max,shp,mid,asy)
+      !--- Growth Function with asymetry (optional)
+      !--- Asymptote Curve and its first derivative (df=1)
+      !--- Author: Murilo S. Vianna
+      !--- Feb-2018
+      
+      implicit none
+      
+      integer df    ! Equation form: 0 = integral, 1 = 1st derivative
+      real  x       ! X Value
+      real  min     ! Minimum Value
+      real  max     ! Maximum Value
+      real  shp     ! Function Shape
+      real  mid     ! X Value in which y = (max - min) / 2.
+      real  asy     ! Asymetry
+      
+      select case(df)
+      case(0)
+          !Integral form
+          AScurv = max + ((min - max) / ((1.d0 + (x/mid)**shp)**asy))
+          
+      case(1)
+          !1st derivative
+          AScurv = -asy * (min-max) * ((1.d0+(x/mid)**shp)**(-asy-1.d0)) * (shp*(x/mid)**(shp-1.d0)) * (1.d0/mid)
+          
+      end select 
+      return
+      end
+!----------------------------------------------------------------------------------------
